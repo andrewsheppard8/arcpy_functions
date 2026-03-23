f6ac306d200b9fc454603ed376bf069fe2c9b243
Andrew Sheppard <andrewsheppard8@gmail.com>
2026-03-23 16:08:56 -0700

    Updating python files

---
+++ Creating_Opening Map in Current Project.py
@@ -0,0 +1,33 @@
+"""
+===============================================================================
+Script Name:       Creating/Opening a Map in a Project
+Author:            Andrew Sheppard
+Role:              GIS Solutions Engineer
+Email:             asheppard@mgt.us
+Date Created:      2026-03-23
+
+Description:
+-------------
+1. Will check to see if a map with a name of map_name exists in the open project.
+2. Will open the map in the current aprx.
+===============================================================================
+"""
+#function 1
+def ensure_map_exists(map_name):
+    aprx_path='CURRENT'
+    aprx=arcpy.mp.ArcGISProject(aprx_path)
+    for map_obj in aprx.listMaps(map_name):
+        aprx.deleteItem(map_obj)
+        arcpy.AddMessage(f"{map_name} map exists, deleted")
+    map_obj = aprx.createMap(map_name)
+    arcpy.AddMessage(f"{map_name} map created")
+
+    return map_obj
+
+#function 2
+def set_active_view(map_name):
+    aprx_path = 'CURRENT'
+    aprx = arcpy.mp.ArcGISProject(aprx_path)
+    map_view = aprx.listMaps(map_name)[0]
+    map_view.openView()
+    arcpy.AddMessage(f"Set {map_name} as the active view")
